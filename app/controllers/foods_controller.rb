73974ff110541c39e63f422ae6295590f8a3acff
MNisarAli <dr.nisaralig@gmail.com>
2023-07-04 16:47:15 +0500

    Create the Food controller

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,10 @@
+class FoodsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get 'foods/new'
+  get 'foods/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
