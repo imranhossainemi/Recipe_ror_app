2892554f7bdf71adb825408c65bd4f902e536c56
Imran Hossain <blur-imran@hotmail.com>
2023-07-05 17:17:38 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'recipes/index', to: 'recipes#index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "recipes#index"
 end
