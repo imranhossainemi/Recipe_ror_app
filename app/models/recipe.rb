1d27afff4a0c9e4b18083afd74c0aaa28d3b5de9
Imran Hossain <blur-imran@hotmail.com>
2023-07-07 03:18:36 +0600

    Update recipe.rb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-
+  has_many :recipe_foods, dependent: :destroy
+  has_many :foods, through: :recipe_foods
+  
   validates :name, presence: true, length: { in: 1..18 }
   validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
