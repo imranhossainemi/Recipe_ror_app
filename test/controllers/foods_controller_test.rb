342cf172681b0c8a9089827727563471e7ec467a
MNisarAli <dr.nisaralig@gmail.com>
2023-07-06 17:17:01 +0500

    Update the Food model and set up associations

--- test/controllers/foods_controller_test.rb
+++ test/controllers/foods_controller_test.rb
@@ -1,18 +1,7 @@
 require "test_helper"
 
 class FoodsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get foods_index_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get foods_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get foods_create_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
