ba52ac48656003ad504e51152b37b4237e870347
Imran Hossain <blur-imran@hotmail.com>
2023-07-06 12:00:02 +0600

    Revert "Add login"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
-  root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
