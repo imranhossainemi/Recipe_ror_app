05fee4d7fe209d051b02ad35a92f5e132d752490
MNisarAli <dr.nisaralig@gmail.com>
2023-07-06 14:53:52 +0500

    Set up devise for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :foods, only: [:index, :new, :create]
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
