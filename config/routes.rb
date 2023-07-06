f991b765e1180130a4c490999e3a8a8650d184be
Imran Hossain <blur-imran@hotmail.com>
2023-07-06 15:53:21 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'public_recipes#index'
- 
+  # root to: "home#index"
    resources :recipes, only: [:index, :show, :create, :new, :destroy] do
      resources :recipe_foods, except: :update
    end
